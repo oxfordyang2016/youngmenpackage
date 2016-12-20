4bc541150b290a40b4b923a5ecadde922fdee941
oxfordyang2016 <yang756260386@gmail.com>
2016-12-20 11:54:54 +0800

    allok

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'youngmenpackage',
   packages = ['youngmenpackage'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = 'A tutorial lib',
   author = 'yangming',
   author_email = 'yang756260386@gmail.com',
   url = 'https://github.com/oxfordyang2016/youngmenpackage',
-  download_url = 'https://github.com/oxfordyang2016/youngmenpackage/tarball/0.1',
+  download_url = 'https://github.com/oxfordyang2016/youngmenpackage/tarball/0.2',
   keywords = ['add', 'sub', 'tests'], 
   classifiers = [],
 )
